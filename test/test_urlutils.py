63d28e9fa2724e023c1d4931b32335fa0cf6fe28
Sandro Tosi <morph@debian.org>
2011-06-24 21:19:48 +0200

    add 'network' attribute (so it can be skipped in quicktest)

--- test/test_urlutils.py
+++ test/test_urlutils.py
@@ -1,9 +1,12 @@
 import unittest2
 
+from nose.plugins.attrib import attr
+
 from reportbug import urlutils
 
 class TestNetwork(unittest2.TestCase):
 
+    @attr('network') #mark the test as using network
     def test_open_url(self):
 
         page = urlutils.open_url('http://bugs.debian.org/reportbug')
