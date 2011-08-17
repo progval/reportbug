e600e1865538fa219ea9859718c591c6e02d3265
Sandro Tosi <morph@debian.org>
2011-08-17 16:14:07 +0200

    releasing version 6.1

--- reportbug/__init__.py
+++ reportbug/__init__.py
@@ -25,7 +25,7 @@ SOFTWARE."""
 __all__ = ['bugreport', 'utils', 'urlutils', 'checkbuildd', 'checkversions',
            'debbugs', 'exceptions', 'submit', 'tempfile']
 
-VERSION_NUMBER = "6.0"
+VERSION_NUMBER = "6.1"
 
 VERSION = "reportbug "+VERSION_NUMBER
 COPYRIGHT = VERSION + '\nCopyright (C) 1999-2008 Chris Lawrence <lawrencc@debian.org>' + \
