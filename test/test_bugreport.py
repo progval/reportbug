47d5a62059bed4d0ba3f5a035c46169e7a2fbc3b
Sandro Tosi <morph@debian.org>
2011-10-04 23:13:58 +0200

    add test for #643785

--- test/test_bugreport.py
+++ test/test_bugreport.py
@@ -1,6 +1,7 @@
 import unittest2
 
 from reportbug import utils
+from reportbug import debbugs
 from reportbug.bugreport import bugreport
 from nose.plugins.attrib import attr
 import debianbts
@@ -18,6 +19,13 @@ class TestBugreport(unittest2.TestCase):
         self.assertIn(self.body, self.text)
         self.assertIn(self.package, self.text)
         self.assertIn(utils.NEWBIELINE, self.text)
+
+    # verify that for special packages, we don't add the report template
+    def test_bts643785(self):
+        for package in debbugs.SYSTEMS['debian'].get('specials', {}).keys():
+            self.report = bugreport(package=package, mode=utils.MODE_NOVICE)
+            self.text = self.report.__unicode__()
+            self.assertNotIn(utils.NEWBIELINE, self.text)
 
     @attr('network') #marking the test as using network
     def test_followup(self):
